e5af3f61eda1095ba9046b1ab0c20e43addcad20
Jenny Vallon <jennyvallon@gmail.com>
2017-06-12 12:00:49 -0400

    shell: add global styles (#3)
    * current changes
    
    * color stop using linear gradient
    
    * css cleanup; removed unnecessary css
    
    * presented two solutions to background gradient; plus more css clean up
    
    * css clean up
    
    * fixed sticky footer bug. make sure html height is 100% to work.
    
    * shell: add global styles
    
    Added global styles
    
    Fixes: CNNENTANTWO-46
    
    Reviewed-by:Jenny Vallon <jenny.vallon@turner.com>
    
    * styles: updated global styles
    
    Cleaning up shell/styles.css.
    
    * made code pass eslint tests

--- src/modules/header/index.js
+++ src/modules/header/index.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import styles from './styles.css';
 
+
 const Header = ({ links }) => (
-    <header className={styles.header}>
-        <Link to="/" className={styles.brand} />
-        <nav className={styles.nav}>
-            {links.map((link) => (
-                <NavLink
-                    key={link.path}
-                    to={link.path}
-                    activeClassName="active"
-                    className={styles.link}
-                >
-                    {link.displayText}
-                </NavLink>
-            ))}
-        </nav>
-    </header>
+    <div>
+        <header className={styles.header}>
+            <Link to="/" className={styles.brand}/>
+            <nav className={styles.nav}>
+                {links.map((link) => (
+                    <NavLink
+                        key={link.path}
+                        to={link.path}
+                        activeClassName="active"
+                        className={styles.link}
+                    >
+                        {link.displayText}
+                    </NavLink>
+                ))}
+            </nav>
+        </header>
+    </div>
 );
 
 export default Header;
--- src/modules/shell/index.js
+++ src/modules/shell/index.js
@@ -11,25 +11,30 @@ import Header from 'modules/header';
 import Footer from 'modules/footer';
 // global application styles
 import styles from 'modules/shell/styles.css';
+// HEADER REMAINS INSIDE BECAUSE IT DEPENDS ON CONTAINER WIDTH FOR CENTERING OF IT'S CONTENTS
 
 function App() {
     return (
-        <div>
-            <Helmet
-                titleTemplate="%s - CNN.com"
-                defaultTitle="CNN Apple News Tools Factors Front End"
-            >
-                <meta name="description" content="CNN Apple News Tools Factors Front End" />
-            </Helmet>
-            <Header links={navigation} />
-            <Card className={styles.foo}>hi</Card>
-            <Switch>
-                {routes.map((route, i) => (
-                    <Route key={i} {...route} />
-                ))}
-            </Switch>
-            <Footer />
-        </div>
+        <shell-container>
+
+            <content-container>
+                <Helmet
+                    titleTemplate="%s - CNN.com"
+                    defaultTitle="CNN Apple News Tools Factors Front End"
+                >
+                    <meta name="description" content="CNN Apple News Tools Factors Front End" />
+                </Helmet>
+                <Header links={navigation} />
+                <h1>Factors</h1>
+                <Card className={styles.paper}></Card>
+                <Switch>
+                    {routes.map((route, i) => (
+                        <Route key={i} {...route} />
+                    ))}
+                </Switch>
+            </content-container>
+            <Footer/>
+        </shell-container>
     );
 }
 
